056d91f821a35a20dade6322efb585eeb3401b6c
Nikhil Deo <89142086+Nikhildevnikhil@users.noreply.github.com>
2022-05-21 23:50:54 +0530

    Create Roman Number to Integer.cpp

---
+++ String/Roman Number to Integer.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+        unordered_map<char,int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        int prev = 0 , ans = 0;
+        for(int i = str.size()-1 ; i >=0 ; i--){
+            if(m[str[i]] >= prev){
+                ans += m[str[i]];
+                prev = m[str[i]];
+            }
+            else{
+                ans -= m[str[i]];
+                }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.romanToDecimal(s) << endl;
+    }
+}  // } Driver Code Ends
